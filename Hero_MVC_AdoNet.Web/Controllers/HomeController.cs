0234f8d8ed5e801bc4af699da6b97f8ccfb108ee
Paulo de Tarso <potassio@hotmail.com>
2023-08-15 11:51:51 -0300

    Add project files.

---
+++ Hero_MVC_AdoNet.Web/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Hero_MVC_AdoNet.Web.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Hero_MVC_AdoNet.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Hero_MVC_AdoNet.Web/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Hero_MVC_AdoNet.Web.ViewModels
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
