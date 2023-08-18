92f68e8ca8548b518aa267b024d5acc0d361cafa
Paulo de Tarso <potassio@hotmail.com>
2023-08-17 21:38:04 -0300

    Adição do método GetAll no repositório de heróis

--- Hero_MVC_AdoNet.DAL/Repositories/HeroRepository.cs
+++ Hero_MVC_AdoNet.DAL/Repositories/HeroRepository.cs
@@ -2,6 +2,10 @@
 using Hero_MVC_AdoNet.DAL.Repositories.Interfaces;
 using Hero_MVC_AdoNet.Domain.Models;
 using Microsoft.Extensions.Options;
+using System.Data.SqlClient;
+using System.Data;
+using System.Reflection;
+using System;
 
 namespace Hero_MVC_AdoNet.DAL.Repositories
 {
@@ -16,7 +20,39 @@ namespace Hero_MVC_AdoNet.DAL.Repositories
 
         public List<Hero> GetAll()
         {
-            throw new NotImplementedException();
+            List<Hero> result = new();
+            SqlCommand command = new("dbo.HeroGetAll");
+
+            try
+            {
+                command.Connection = new SqlConnection(_connection.DefaultConnection);
+                command.Connection.Open();
+                command.CommandType = CommandType.Text;
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    result.Add(new Hero
+                    {
+                        HeroId = Convert.ToInt32(reader["HeroId"]),
+                        Name = reader["Name"].ToString(),
+                        Active = Convert.ToBoolean(reader["Active"]),
+                        UpdateDate = Convert.ToDateTime(reader["UpdateDate"])
+                    });
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Falha no repositório. {e.Message} - {e.StackTrace} - {DateTime.Now}");
+                throw new Exception("Erro ao acessar as informações do banco de dados.");
+            }
+            finally
+            {
+                if (command.Connection.State == ConnectionState.Open)
+                    command.Connection.Close();
+            }
+            return result;
         }
 
         public List<Weapon> GetAllPowers()
