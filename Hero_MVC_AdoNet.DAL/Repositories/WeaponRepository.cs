07cf138d3bce218079c916b4f4e92155478752ae
Paulo de Tarso <potassio@hotmail.com>
2023-08-21 19:42:32 -0300

    Adição do método GetById no repositório de armas

--- Hero_MVC_AdoNet.DAL/Repositories/WeaponRepository.cs
+++ Hero_MVC_AdoNet.DAL/Repositories/WeaponRepository.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.Options;
 using System.Data.SqlClient;
 using System.Data;
 using Hero_MVC_AdoNet.Domain.Enum;
+using System.Xml.Linq;
 
 namespace Hero_MVC_AdoNet.DAL.Repositories
 {
@@ -55,7 +56,42 @@ namespace Hero_MVC_AdoNet.DAL.Repositories
 
         public Weapon GetById(int id)
         {
-            throw new NotImplementedException();
+            Weapon result = new();
+            SqlCommand command = new("dbo.WeaponGetById");
+
+            try
+            {
+                command.Connection = new SqlConnection(_connection.DefaultConnection);
+                command.Connection.Open();
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.Add("@WeaponId", SqlDbType.Int).Value = id;
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (!reader.Read())
+                {
+                    if (command.Connection.State == ConnectionState.Open)
+                        command.Connection.Close();
+
+                    throw new Exception("Objeto não encontrado.");
+                }
+
+                result.WeaponId = Convert.ToInt32(reader["WeaponId"]);
+                result.Name = reader["Name"].ToString();
+                result.Type = (WeaponEnum)Convert.ToInt16(reader["Type"]);
+                result.HeroId = Convert.ToInt32(reader["HeroId"]);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Falha no repositório. {e.Message} - {e.StackTrace} - {DateTime.Now}");
+                throw new Exception("Erro ao acessar as informações do banco de dados.");
+            }
+            finally
+            {
+                if (command.Connection.State == ConnectionState.Open)
+                    command.Connection.Close();
+            }
+            return result;
         }
 
         public Hero GetHero(int weaponId)
