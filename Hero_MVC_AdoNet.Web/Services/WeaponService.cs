81c2e29a68a11572a594d6db0674022e839c5f53
Paulo de Tarso <potassio@hotmail.com>
2023-08-23 20:01:30 -0300

    Criada as classes de serviços

---
+++ Hero_MVC_AdoNet.Web/Services/WeaponService.cs
@@ -0,0 +1,21 @@
+﻿using Hero_MVC_AdoNet.DAL.Repositories.Interfaces;
+
+namespace Hero_MVC_AdoNet.Web.Services
+{
+    public class WeaponService
+    {
+        private readonly IHeroRepository _heroRepository;
+        private readonly IMovieRepository _movieRepository;
+        private readonly ISecretRepository _secretRepository;
+        private readonly IWeaponRepository _weaponRepository;
+
+        public WeaponService(IHeroRepository heroRepository, IMovieRepository movieRepository, ISecretRepository secretRepository, IWeaponRepository weaponRepository)
+        {
+            _heroRepository = heroRepository;
+            _movieRepository = movieRepository;
+            _secretRepository = secretRepository;
+            _weaponRepository = weaponRepository;
+        }
+
+    }
+}
