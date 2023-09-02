a45199fc2c64c6495f1186fa45ca28a07776d1f7
Paulo de Tarso <potassio@hotmail.com>
2023-09-02 10:23:47 -0300

    Correção de warnings e criação da página de detalhes de herói

--- Hero_MVC_AdoNet.Web/ViewModels/ErrorViewModel.cs
+++ Hero_MVC_AdoNet.Web/ViewModels/ErrorViewModel.cs
@@ -2,7 +2,7 @@ namespace Hero_MVC_AdoNet.Web.ViewModels
 {
     public class ErrorViewModel
     {
-        public string? RequestId { get; set; }
+        public string RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
