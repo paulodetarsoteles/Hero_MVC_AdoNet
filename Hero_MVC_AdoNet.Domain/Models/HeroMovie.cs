4232f07e38e53bda1303744cbf059f6004418311
Paulo de Tarso <potassio@hotmail.com>
2023-09-09 20:20:13 -0300

    Adição das entidas de relação entre filme e herói

---
+++ Hero_MVC_AdoNet.Domain/Models/HeroMovie.cs
@@ -0,0 +1,9 @@
+﻿namespace Hero_MVC_AdoNet.Domain.Models
+{
+    public class HeroMovie
+    {
+        public int HeroMovieId { get; set; }
+        public int HeroId { get; set; }
+        public int MovieId { get; set; }
+    }
+}
---
+++ Hero_MVC_AdoNet.Web/ViewModels/HeroMovieViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Hero_MVC_AdoNet.Web.ViewModels
+{
+    public class HeroMovieViewModel
+    {
+        public int HeroMovieId { get; set; }
+        public int HeroId { get; set; }
+        public int MovieId { get; set; }
+        public HeroViewModel HeroModel { get; set; }
+        public MovieViewModel MovieModel { get; set; }
+    }
+}
