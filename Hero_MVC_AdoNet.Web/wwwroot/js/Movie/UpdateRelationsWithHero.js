b8f220ae899c4d51ad4721319711f220e5038fe6
Paulo de Tarso <potassio@hotmail.com>
2023-09-13 21:02:31 -0300

    Implantação das funções javascript

--- Hero_MVC_AdoNet.Web/wwwroot/js/Movie/UpdateRelationsWithHero.js
+++ Hero_MVC_AdoNet.Web/wwwroot/js/Movie/UpdateRelationsWithHero.js
@@ -1,1 +1,34 @@
-﻿
+﻿//Edição da relação filme/heróis
+$(document).ready(function () {
+    $("#btnAddHero").click(function () {
+        var chkRptHero = false;
+        var currentValue = $("#HeroId option:selected").text().trim();
+
+        if (currentValue == "Selecione...") {
+            return false;
+        }
+
+        $('#listHeroes li').each(function () {
+            var current = $(this).text().trim();
+
+            if (current == $("#HeroId option:selected").text().trim()) {
+                chkRptHero = true;
+            }
+        });
+
+        if (!chkRptHero) {
+            $("#listHeroes").append(
+                "<li>" +
+                $("#HeroId option:selected").text() +
+                "<input type='hidden' name='chkHero' id='chkHero' class='chkHero' value='" + $("#HeroId option:selected").val() + "'>" +
+                "</li>");
+        } else {
+            alert("Herói já adicionado!");
+        }
+    });
+
+    $('#listHeroes').on('click', "li", function () {
+        $(this).remove();
+        return false;
+    });
+});
